c6ed5802880d39a8fdc1bb2ac1b649fd536705f0
João Gustavo S. Bispo <jgbispo20@gmail.com>
2024-06-22 00:04:47 -0300

    Replace VUE with React
    Apenas foi feita a reconfigução, em vez de usar VUE, vamos usar REACT

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const options = {
+  arrowParens: 'void',
+  singleQuote: true,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  semi: false,
+  tabWidth: 2,
+  trailingComma: 'all',
+}
+
+export default options
